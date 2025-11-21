1ee0c87a9a579bcf5c72d723ad795cae88f46821
Dobrodei <85105792+Dobrodei@users.noreply.github.com>
2025-11-22 01:56:19 +0300

    Add files via upload

---
+++ keys/js/config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+  basePriceVip: 459,
+  basePriceStandart: 359,
+  promoCodes: {
+    KEY20: 0.2,
+    VIP50: 0.5,
+    SUPER10: 0.1,
+  },
+  stripeLink: "https://stripe.com/pay",
+};
